06f4b556d0f35a72f221fc1b724f937be2247222
DejanSGH <120726875+DejanSGH@users.noreply.github.com>
2023-06-05 13:59:31 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ISaveable player;
+//
+//        player = new Player("Tim", 10, 10);
+//        player = new Player("Bob", 20, 20);
+//        player = new Player("Sam", 30, 30);
+//
+//        List myList = new ArrayList<>();
+//
+//        myList.add(player);
+//        myList.add(player);
+//        myList.add(player);
+//
+//        player.read(myList);
+
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        values.add(3, this.weapon);
+
+        return values;
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if (list != null && list.size() > 0) {
+            this.name = list.get(0);
+            this.hitPoints = Integer.parseInt(list.get(1));
+            this.strength = Integer.parseInt(list.get(2));
+            this.weapon = list.get(3);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+}
